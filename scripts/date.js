84348b73184cf02d710f01aa480fcbee04020988
mjgifford3485 <mjgifford3485@gmail.com>
2025-05-15 14:05:16 -0700

    week 1 homepage

---
+++ scripts/date.js
@@ -0,0 +1,9 @@
+const lastModified = document.lastModified;
+
+document.getElementById("lastModified").textContent = lastModified;
+
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
