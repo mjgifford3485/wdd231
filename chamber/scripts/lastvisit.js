709032ecd6f9cee0736ff4505b3f68409eba7b70
mjgifford3485 <mjgifford3485@gmail.com>
2025-06-07 21:31:21 -0700

    testing

--- chamber/scripts/lastvisit.js
+++ chamber/scripts/lastvisit.js
@@ -13,9 +13,7 @@ if (!lastVisit) {
     const hourDiff = Math.floor(timeDiff / (1000 * 60 * 60))
     const dayDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
 
-    if (dayDiff === 0) {
-        message = "Welcome! Let us know if you have any questions.";
-    } else if (hourDiff < 24) {
+    if (hourDiff < 24) {
         message = "Back so soon! Awesome!"
     } else {
         message = `You last visited ${dayDiff} days ago.`
